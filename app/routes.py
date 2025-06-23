942d74f806f67d685bc95682156118d8ddf1c0a6
Ahmed-Osama-Fawzy <207079550+Elkhayal@users.noreply.github.com>
2025-06-23 21:20:34 +0300

    dd

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,14 @@
+from flask import jsonify, request
 from app import app, db
 
 @app.route('/')
 def Home():
     return "Hello"
+
+@app.route("/Login", methods=["POST"])
+def Login():
+    try:
+        if request.form.get("name") == "Ali":
+            return jsonify({"message":"", "status":"success"}), 200
+    except Exception as e:
+        return jsonify({"message":"", "status":"failed"}), 500
